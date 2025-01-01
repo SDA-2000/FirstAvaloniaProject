f6a8a5a80e0e8c35f5dabc7144c92861ef06c151
ToniTrum <kulahszjan@gmail.com>
2025-01-01 12:30:09 +1000

    add templates for MainWindow

---
+++ Views/ControlPanel.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GetStartedApp.Views;
+
+public partial class ControlPanel : UserControl
+{
+    public ControlPanel()
+    {
+        InitializeComponent();
+    }
+}
